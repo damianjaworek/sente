6495aad6829da48b29869f9372eb3c7871338eb1
Damian Jaworek <damianjaworek@outlook.com>
2020-10-10 12:26:37 +0200

    Added required CLI options

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
-use esprimo::test;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "esprimoc")]
+struct CliOptions {
+    #[structopt(short, long, parse(from_os_str))]
+    input: PathBuf,
+    #[structopt(short, long, parse(from_os_str))]
+    output: PathBuf,
+}
 
 fn main() {
-    println!("Hello, world!");
-    test();
+    let cli_options = CliOptions::from_args();
+    println!("{:#?}", cli_options);
 }
